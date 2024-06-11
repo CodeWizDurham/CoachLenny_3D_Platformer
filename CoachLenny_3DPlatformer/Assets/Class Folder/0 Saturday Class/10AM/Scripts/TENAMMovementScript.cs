4234dff21e2d108920fbae33807ff6e69c76cb5f
lsclarke <139919164+CodeWizDurham@users.noreply.github.com>
2024-06-11 00:31:04 -0400

    NEW

--- CoachLenny_3DPlatformer/Assets/Class Folder/0 Saturday Class/10AM/Scripts/TENAMMovementScript.cs
+++ CoachLenny_3DPlatformer/Assets/Class Folder/0 Saturday Class/10AM/Scripts/TENAMMovementScript.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class TENAMMovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Variable that controls our speed
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
+        //If I press the W key
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Move my player forward at a speed of 10f by the second
+            transform.Translate(transform.forward * speed * Time.deltaTime);
+        }
+
+        //If I press the S key
+        //If I press the D key
+        //If I press the A key
+
+        /// Challenge 1
+        /// Create key inputs for all four directions
+        /// Use the example code above to recreate the movement and remember: 
+        /// Positive is: right, up, and forward
+        /// Negavtive is: left, down, and backwards
         
+
+        ///Challenge 2
+        ///Create a if statement when pressed DOWN the player jumps!
+        ///use Input.GetKeyDown()
+        ///use a RigidBody variable to use AddForce() method
+
     }
 }
--- CoachLenny_3DPlatformer/Assets/Coach Folder/Script/SampleMovementScript.cs
+++ CoachLenny_3DPlatformer/Assets/Coach Folder/Script/SampleMovementScript.cs
@@ -14,58 +14,71 @@ public class SampleMovementScript : MonoBehaviour
     //This variable checks if were on the ground
     public bool isGrounded;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        //3d
-        //if i press w
+        /// Challenge
+        /// Create key inputs for all four directions
+        /// Use the example code above to recreate the movement and remember: 
+        /// Positive is: right, up, and forward
+        /// Negavtive is: left, down, and backwards
+        
+        //If I press the W key
         if (Input.GetKey(KeyCode.W))
         {
-            //ove my player forward
+            //Move my player forward at a speed of 10f by the second
             transform.Translate(transform.forward * speed * Time.deltaTime);
         }
 
+        //If I press the S key
         if (Input.GetKey(KeyCode.S))
         {
+            //Move my player backward at a speed of 10f by the second
             transform.Translate(transform.forward * -1 * speed * Time.deltaTime);
         }
 
+        //If I press the D key
         if (Input.GetKey(KeyCode.D))
         {
+            //Move my player right at a speed of 10f by the second
             transform.Translate(transform.right * speed * Time.deltaTime);
         }
 
+        //If I press the A key
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(transform.forward * -1 * speed * Time.deltaTime);
+            //Move my player left at a speed of 10f by the second
+            transform.Translate(transform.right * -1 * speed * Time.deltaTime);
         }
 
+        ///Challenge 2
+        ///Create a if statement when pressed DOWN the player jumps!
+        ///use Input.GetKeyDown()
+        ///use a RigidBody variable to use AddForce() method
+
+        //If I press the space key and i am grounded
         if (Input.GetKey(KeyCode.Space) & isGrounded)
         {
-            //Player Jump
+            //Create a local Vector3 variable and put the jump variable in the Y column
             Vector3 JumpForce = new Vector3(physics.velocity.x, jump, physics.velocity.z);
-            physics.AddForce(JumpForce,ForceMode.Force);
+
+            //Create a Force that causes the player to jump
+            physics.AddForce(JumpForce, ForceMode.Force);
         }
+
     }
 
+    //When player collides with an object
     private void OnCollisionEnter(Collision other)
     {
         isGrounded = true;
-
-        if (other.gameObject.tag == "ground")
-        {
-
-        }
     }
 
+    //When player leaves an object
     private void OnCollisionExit(Collision collision)
     {
         isGrounded = false;
     }
+
+
 }
